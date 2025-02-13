45d1610f665e341fbc7f24933004264a38b1bdd9
Rumbar <diogo.ramossr@gmail.com>
2025-02-13 10:31:19 -0300

    DIA3

--- Projeto2/Program.cs
+++ Projeto2/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace course
+﻿using System.Globalization;
+namespace course
 {
     class Program{ 
         static void Main(string[] args) {
@@ -6,7 +7,10 @@
             //aula2();
             //aula3();
             //aula4();
-            aula5();
+            //aula5();
+            //aula6();
+            //exercicio1();
+            //aula7();
 
         }
 
@@ -113,7 +117,75 @@
 
 
         }
+        static void aula6(){
 
+            int n1= int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+            Console.WriteLine("Você Digitou");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2);
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura);
+
+       }   
+
+        static void exercicio1(){
+
+            Console.WriteLine("Entre com seu nome completo:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Quantos carros existem na sua casa");
+            int quartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto");
+            double prod = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre seu último nome, idade e altura");
+            string[] vet = Console.ReadLine().Split(' ');
+            string nomemeio= vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura= double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(prod);
+            Console.WriteLine(nomemeio);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura);
+        }
+        static void aula7(){
+
+            int a = 10;
+            bool c1 = a < 10;
+            bool c2 = a < 20;
+            bool c3 = a > 10;
+            bool c4 = a > 5;
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+            Console.WriteLine(c4);
+            Console.WriteLine("-----------------");
+
+            bool c5 = a <= 10;
+            bool c6 = a >= 10;
+            bool c7 = a == 10;
+            bool c8 = a != 10;
+            Console.WriteLine(c5);
+            Console.WriteLine(c6);
+            Console.WriteLine(c7);
+            Console.WriteLine(c8);
+
+
+        }
 
 
     }
